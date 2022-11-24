98969b9dd810a6c9d51da19d2f86ddcf889a7482
Vamilton <arteiano@yandex.ru>
2022-11-24 10:56:53 +0300

    del

--- test_accountancy.py
+++ test_accountancy.py
@@ -32,15 +32,18 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(show_shelf_by_doc(documents, directories), shelf)
 
     def test_all_documents(self):
-        self.assertEqual(all_documents(documents, directories), ['passport "2207 876234" "Василий Гупкин"', 'invoice "11-2" "Геннадий Покемонов"', 'insurance "10006" "Аристарх Павлов"'])
+        self.assertEqual(all_documents(documents, directories), ['passport "2207 876234" "Василий Гупкин"', 'invoice "11-2" "Геннадий Покемонов"', 'insurance "10006" "Аристарх Павлов"', 'passport "101202" "Максим"'])
 
 
+#тут проблема, потому что я не знаю, как замокать цикличный ввод
     @patch('builtins.input')
-    def test_add_new_doc1(self, m_input):
+    def test_add_new_doc(self, m_input):
         m_input.side_effect = ['passport', '101202', 'Максим', '1']
         self.assertEqual(add_new_doc(documents, directories), 'Успешно помещено на полку 1')
 
-
+    def delete_doc(self): #очень удобно, тестирование этой функции удалит тестовый документ
+        with patch('accountancy.input', return_value='101202') as _raw_input:
+            self.assertEqual(delete_doc(documents, directories), 'Удалено')
             
 if __name__ == '__main__':
     unittest.main()
